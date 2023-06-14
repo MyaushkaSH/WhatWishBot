9cd861373e42d33fc877d46f2daeefe6f5c3c3b4
MyaushkaSH <43733997+MyaushkaSH@users.noreply.github.com>
2023-06-15 00:47:17 +0300

    Little log

--- src/main/java/com/myaushka/whatwishbot/Greeting.java
+++ src/main/java/com/myaushka/whatwishbot/Greeting.java
@@ -1,15 +1,16 @@
 package com.myaushka.whatwishbot;
 
 import lombok.Getter;
-import org.springframework.data.annotation.Id;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Getter
 public class Greeting {
     private final long id;
     private final String content;
-
     public Greeting(long id, String content) {
         this.id = id;
         this.content = content;
     }
+
 }
--- src/main/java/com/myaushka/whatwishbot/telegram/WhatWishBotHeart.java
+++ src/main/java/com/myaushka/whatwishbot/telegram/WhatWishBotHeart.java
@@ -1,6 +1,8 @@
 package com.myaushka.whatwishbot.telegram;
 
 import com.myaushka.whatwishbot.Greeting;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +13,11 @@ import java.util.concurrent.atomic.AtomicLong;
 public class WhatWishBotHeart {
     private static final String template = "AAAAAA, %s?";
     private final AtomicLong counter = new AtomicLong();
+    private final Logger logger = LoggerFactory.getLogger(WhatWishBotHeart.class);
 
     @RequestMapping("/")
     public Greeting greeting(@RequestParam(value="name", required=false, defaultValue="WAZZZAAAAAAAAP!!!!") String name) {
+        logger.info("Прилетел запрос");
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
